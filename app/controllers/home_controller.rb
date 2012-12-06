9374ca7d3f4b2688d65fb3c1e8863c10fd596928
Zahra Jabini <zahra.arch@gmail.com>
2012-12-06 14:11:08 -0500

    added get data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,13 +6,15 @@ class HomeController < ApplicationController
     Building.delete_all
     x = HTTTParty.get('http://data.cityofnewyork.us/api/views/74cu-ncm4/rows.json')
     y = JSON(x.body)
-    lat = y["data"].first[8][1]
-    long = y["data"].first[8][2]
-    building_type = y["data"].first[9]
-    consumption = y["data"].first[10]
-    zip = JSON(y["data"][0][8][0])['zip']
-
-
+    y["data"].each do |d|
+      lat = d.first[8][1]
+      long = d.first[8][2]
+      building_type = d.first[9]
+      consumption = d.first[10]
+      zip = JSON(d[0][8][0])['zip']
+      building = Building.create(:building_type => building_type, :lat => lat, :long => long, :kwh => consumption, :zipcode => zip)
+    end
+    @buildings = Building.all
   end
 
 
