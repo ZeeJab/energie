f466264f742ad9ad2231bf33cf04de0acef07e95
Zahra Jabini <zahra.arch@gmail.com>
2012-12-06 14:32:21 -0500

    controller updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,18 +4,22 @@ class HomeController < ApplicationController
 
   def data
     Building.delete_all
-    x = HTTTParty.get('http://data.cityofnewyork.us/api/views/74cu-ncm4/rows.json')
+    x = HTTParty.get('http://data.cityofnewyork.us/api/views/74cu-ncm4/rows.json')
+    
+    #binding.pry
+
     y = JSON(x.body)
+
     y["data"].each do |d|
       lat = d.first[8][1]
       long = d.first[8][2]
       building_type = d.first[9]
-      consumption = d.first[10]
-      zip = JSON(d[0][8][0])['zip']
+      consumption = d.first[10] 
+      zip = d[8][0].split('"')[-2] if d[8][0]
       building = Building.create(:building_type => building_type, :lat => lat, :long => long, :kwh => consumption, :zipcode => zip)
     end
     @buildings = Building.all
+    redirect_to root_path
   end
-
 
 end
