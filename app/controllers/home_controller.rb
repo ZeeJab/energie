4d9e887f30b93b4c3fcdc43073aeef8e8a7e0339
Zahra Jabini <zahra.arch@gmail.com>
2012-12-06 14:37:53 -0500

    controller fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,10 +11,10 @@ class HomeController < ApplicationController
     y = JSON(x.body)
 
     y["data"].each do |d|
-      lat = d.first[8][1]
-      long = d.first[8][2]
-      building_type = d.first[9]
-      consumption = d.first[10] 
+      lat = d[8][1]
+      long = d[8][2]
+      building_type = d[9]
+      consumption = d[10] 
       zip = d[8][0].split('"')[-2] if d[8][0]
       building = Building.create(:building_type => building_type, :lat => lat, :long => long, :kwh => consumption, :zipcode => zip)
     end
